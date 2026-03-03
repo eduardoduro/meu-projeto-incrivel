551cf213f628ac7042952c65ca552879d9732a45
eduardoduro <eduardoduro06@gmail.com>
2026-03-03 12:19:27 -0300

    criei novos projetos

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 from flask import Flask
-from flask_sqlalchemy import SQALchemy
-from conf.database import i
+from configuracao.database import init_db
+
+#Importar módulos 
+from control.marca import marca_bp
+from control.produto import produto_bp
+
+
+app = Flask(__name__)
+
+#Conexao Geral do meu app
+init_db(app)
+
+#Registro de controladores 
+app.register_blueprint(marca_bp)
+app.register_blueprint(produto_bp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
